4c7b02d2f4e4a4d79e326da5195dfb8bbb32c028
Ahnik <sarkarahnik@gmail.com>
2026-07-23 18:57:47 +0530

    started working on the packet struct

---
+++ assignment-1/src/sender/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "common.h"
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        printf("Usage: ./main.out <filename>\n");
+        return -1;
+    }
+
+    return 0;
+}
